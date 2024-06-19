6f7e5ff1b84678a8a7e18e043aff172b0ef8bd0a
ROG <luan.roger.2003@gmail.com>
2024-06-19 15:42:51 -0300

    feat: add editor's components

---
+++ src/components/plate-ui/code-block-combobox.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import React, { useState } from 'react';
+
+import { cn } from '@udecode/cn';
+import {
+  CODE_BLOCK_LANGUAGES,
+  CODE_BLOCK_LANGUAGES_POPULAR,
+  useCodeBlockCombobox,
+  useCodeBlockComboboxState,
+} from '@udecode/plate-code-block';
+
+import { Icons } from '@/components/Icons';
+
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+
+const languages: { label: string; value: string }[] = [
+  { label: 'Plain Text', value: 'text' },
+  ...Object.entries({
+    ...CODE_BLOCK_LANGUAGES_POPULAR,
+    ...CODE_BLOCK_LANGUAGES,
+  }).map(([key, val]) => ({
+    label: val as string,
+    value: key,
+  })),
+];
+
+export function CodeBlockCombobox() {
+  const state = useCodeBlockComboboxState();
+  const { commandItemProps } = useCodeBlockCombobox(state);
+
+  const [open, setOpen] = useState(false);
+
+  if (state.readOnly) return null;
+
+  return (
+    <Popover onOpenChange={setOpen} open={open}>
+      <PopoverTrigger asChild>
+        <Button
+          aria-expanded={open}
+          className="h-5 justify-between px-1 text-xs"
+          role="combobox"
+          size="sm"
+          variant="ghost"
+        >
+          {state.value
+            ? languages.find((language) => language.value === state.value)
+                ?.label
+            : 'Plain Text'}
+          <Icons.chevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search language..." />
+          <CommandEmpty>No language found.</CommandEmpty>
+
+          <CommandList>
+            {languages.map((language) => (
+              <CommandItem
+                className="cursor-pointer"
+                key={language.value}
+                onSelect={(_value) => {
+                  commandItemProps.onSelect(_value);
+                  setOpen(false);
+                }}
+                value={language.value}
+              >
+                <Icons.check
+                  className={cn(
+                    'mr-2 size-4',
+                    state.value === language.value ? 'opacity-100' : 'opacity-0'
+                  )}
+                />
+                {language.label}
+              </CommandItem>
+            ))}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
