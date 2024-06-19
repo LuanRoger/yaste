e0cd6a94dd7fe5f853c5210e166cb671648c82b7
ROG <luan.roger.2003@gmail.com>
2024-06-19 15:41:41 -0300

    feat: add editor icons and hook

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export const useDebounce = <T>(value: T, delay = 500) => {
+    const [debouncedValue, setDebouncedValue] = React.useState(value);
+
+    React.useEffect(() => {
+        const handler: NodeJS.Timeout = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+};
